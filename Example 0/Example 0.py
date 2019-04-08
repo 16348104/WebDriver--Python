1e423e3edcdb9ef334f7a7151ed3ae1de789932e
16348104 <16348104@qq.com>
2019-04-08 19:18:26 +0800

    update

--- Example 0/Example 0.py
+++ Example 0/Example 0.py
@@ -1,6 +1,10 @@
+from lib2to3.pgen2 import driver
+
 from selenium import webdriver
 
 # browser = webdriver.Chrome()
-browser=webdriver.Firefox()
-
-browser.get('http://learn2018.tsinghua.edu.cn')
+# browser = webdriver.Firefox()
+browser = webdriver.Ie()
+browser.get('http://www.baidu.com')
+driver.find_element_by_id("kw").send_keys("selenium2")
+driver.find_element_by_id("su").click()
