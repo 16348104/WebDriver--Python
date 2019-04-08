8cc89aeb6bcf0c13ec0fb6b9668419285b81392d
16348104 <16348104@qq.com>
2019-04-08 19:22:10 +0800

    update

--- Example 0/Example 0.py
+++ Example 0/Example 0.py
@@ -2,8 +2,8 @@ from lib2to3.pgen2 import driver
 
 from selenium import webdriver
 
-# browser = webdriver.Chrome()
-# browser = webdriver.Firefox()
+browser = webdriver.Chrome()
+browser = webdriver.Firefox()
 browser = webdriver.Ie()
 browser.get('http://www.baidu.com')
 driver.find_element_by_id("kw").send_keys("selenium2")
