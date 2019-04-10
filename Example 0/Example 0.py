260aa9027f787c8678206c34521ac5a20ce962aa
xiaodaxing <16348104@qq.com>
2019-04-10 11:03:48 +0800

    mac os

--- Example 0/Example 0.py
+++ Example 0/Example 0.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 import time
 # browser = webdriver.Chrome()
-browser = webdriver.Firefox()
+# browser = webdriver.Firefox()
 # browser = webdriver.Ie()
+browser = webdriver.Firefox(executable_path = '/Users/xiaodaxing/Downloads/PycharmProjects/Example 0/geckodriver')  #mac firefox
+# browser = webdriver.Chrome(executable_path = '/Users/xiaodaxing/Downloads/PycharmProjects/Example 0/chromedriver')  #mac os
 browser.get('http://www.baidu.com')
 # browser.get(r'E:\163study\WebDriver--Python\upload.html')  # 文件的地址
 # browser.find_element_by_name('file').send_keys(r'E:/map.png')  # 上传文件
