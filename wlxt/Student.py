e72581e88a983868afbd0521b617a54915623118
16348104 <16348104@qq.com>
2019-04-10 16:56:20 +0800

    update

--- wlxt/Student.py
+++ wlxt/Student.py
@@ -18,6 +18,11 @@ time.sleep(40)#休眠40秒
 # browser.find_element_by_id('loginButtonId').click()
 
 browser.get('http://learn.tsinghua.edu.cn/f/wlxt/index/course/student/course?wlkcid=2018-2019-226ef84e7689589e901689906e324686a')
+time.sleep(1)
+browser.get('http://learn.tsinghua.edu.cn/f/wlxt/kcgg/wlkc_ggb/student/beforePageListXs?wlkcid=2018-2019-226ef84e7689589e901689906e324686a&sfgk=0')
+browser.find_element_by_link_text('new').click()
+browser.find_element_by_id('wjmc').click()
+time.sleep(5)
 
 
 # browser.get(r'E:\163study\WebDriver--Python\upload.html')  # 文件的地址
