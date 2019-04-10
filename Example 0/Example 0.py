205e058531bc3b6fbf720051cd6ab84cec970c7f
16348104 <16348104@qq.com>
2019-04-10 10:23:12 +0800

    update

--- Example 0/Example 0.py
+++ Example 0/Example 0.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 import time
-browser = webdriver.Chrome()
-# browser = webdriver.Firefox()
+# browser = webdriver.Chrome()
+browser = webdriver.Firefox()
 # browser = webdriver.Ie()
-# browser.get('http://www.baidu.com')
-browser.get(r'E:\163study\WebDriver--Python\upload.html')  # 文件的地址
+browser.get('http://www.baidu.com')
+# browser.get(r'E:\163study\WebDriver--Python\upload.html')  # 文件的地址
 # browser.find_element_by_name('file').send_keys(r'E:/map.png')  # 上传文件
 # print('文件上传完毕')
 # browser.find_element_by_class_name()
-time.sleep(5)
+time.sleep(10)
 browser.quit()
