33651c5668e74979fd3a1fa6ad47e1ad3e7d9f33
16348104 <16348104@qq.com>
2019-04-10 10:12:39 +0800

    update

--- Example 0/Example 0.py
+++ Example 0/Example 0.py
@@ -5,8 +5,8 @@ browser = webdriver.Chrome()
 # browser = webdriver.Ie()
 # browser.get('http://www.baidu.com')
 browser.get(r'E:\163study\WebDriver--Python\upload.html')  # 文件的地址
-browser.find_element_by_name('file').send_keys(r'E:/map.png')  # 上传文件
-print('文件上传完毕')
-browser.find_element_by_class_name()
+# browser.find_element_by_name('file').send_keys(r'E:/map.png')  # 上传文件
+# print('文件上传完毕')
+# browser.find_element_by_class_name()
 time.sleep(5)
 browser.quit()
