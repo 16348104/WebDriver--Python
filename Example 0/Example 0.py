e2a69fe513ca53e3cfbb55f89b57b415969fb50e
xiaodaxing <16348104@qq.com>
2019-04-10 17:36:04 +0800

    mac os  safari

--- Example 0/Example 0.py
+++ Example 0/Example 0.py
@@ -1,22 +1,23 @@
 from selenium import webdriver
 import time
 
-browser = webdriver.Chrome()
+# browser = webdriver.Chrome()
 # browser = webdriver.Firefox()
 # browser = webdriver.Ie()
 # browser = webdriver.Firefox(executable_path = '/Users/xiaodaxing/Downloads/PycharmProjects/Example 0/geckodriver')  #mac firefox
 # browser = webdriver.Chrome(executable_path = '/Users/xiaodaxing/Downloads/PycharmProjects/Example 0/chromedriver')  #mac os
-# browser = webdriver.safari()
-browser.get('http://learn.tsinghua.edu.cn')
-browser.find_element_by_name('i_user').clear()
-browser.find_element_by_name('i_pass').clear()
+browser = webdriver.safari()
+browser.get('http://www.baidu.com')
+# browser.find_element_by_name('i_user').clear()
+# browser.find_element_by_name('i_pass').clear()
 # 键入用户名
-browser.find_element_by_name('i_user').send_keys('2016012872')
-# 键入密码
-browser.find_element_by_name('i_pass').send_keys('aihailin0928')
-browser.find_element_by_id('loginButtonId').click()
-time.sleep(2)
-browser.get('http://learn.tsinghua.edu.cn/f/wlxt/index/course/student/course?wlkcid=2018-2019-226ef84e7689589e901689906e324686a')
+# browser.find_element_by_name('i_user').send_keys('2016012872')
+# # 键入密码
+# browser.find_element_by_name('i_pass').send_keys('aihailin0928')
+# browser.find_element_by_id('loginButtonId').click()
+# time.sleep(2)
+# browser.get('http://learn.tsinghua.edu.cn/f/wlxt/index/course/student/course?wlkcid=2018-2019-226ef84e7689589e901689906e324686a')
+
 time.sleep(10)
 # 取ID为txtPwd的网页元素(密码输入元素)
 # elem_pass=driver.find_element_by_id('txtPwd')
