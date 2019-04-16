97e5b096ba0e2465aaf64367a2163a0839bb1094
16348104 <16348104@qq.com>
2019-04-16 11:22:37 +0800

    update  课程文件

--- wlxt/Student.py
+++ wlxt/Student.py
@@ -16,10 +16,11 @@ driver.find_element_by_name('i_user').clear()
 driver.find_element_by_name('i_pass').clear()
 driver.find_element_by_name('i_user').send_keys('2016012872')  # 键入用户名
 driver.find_element_by_name('i_pass').send_keys('aihailin0928')  # 键入密码
-driver.find_element_by_id('loginButtonId').click()
+driver.find_element_by_id('loginButtonId').send_keys(Keys.ENTER)
 # 进入课程【第二个窗口】
-# driver.find_element('//*[@id="suoxuecourse"]/dd/div[2]/div[1]/a')
-driver.find_element_by_link_text('基于Linux的C++(20740084-998)').click()
+# driver.find_element('//*[@id="suoxuecourse"]/dd[5]/div[2]/div[1]/a').click()
+# driver.find_element_by_link_text('基于Linux的C++(20740084-998)').click()
+driver.find_element_by_xpath('//*[@id="suoxuecourse"]//a[contains(text(),"基于Linux的C++(20740084-998)")]').click()
 time.sleep(3)  # 休眠
 # 使用第二种方法切换浏览器【切换到第二个窗口】
 window_1 = driver.current_window_handle
@@ -30,43 +31,41 @@ for current_window in windows:
 time.sleep(3)
 # driver.get( 'http://learn.tsinghua.edu.cn/f/wlxt/index/course/student/course?wlkcid=2018-2019-226ef84e7689589e901689906e324686a')
 
-print("===========测试课程公告============")
-# # 浏览公告#
-# driver.get(
-#     'http://learn.tsinghua.edu.cn/f/wlxt/kcgg/wlkc_ggb/student/beforePageListXs?wlkcid=2018-2019-226ef84e7689589e901689906e324686a&sfgk=0')
+# print("===========测试课程公告============")
+# driver.find_element_by_link_text('课程公告').click()
 # driver.find_element_by_xpath(".//*[@id='table']/tbody/tr[1]/td[1]/a").click()
 # time.sleep(1)
 # driver.find_element_by_id('backBtn').click()
 
 # 课程邮件#
-print('======测试课程邮件=====')
-driver.find_element_by_link_text('课程邮件').click()
-# driver.get( "http://learn.tsinghua.edu.cn/f/wlxt/mail/yj_yjxxb/student/beforePageList?wlkcid=2018-2019-226ef84e7689589e901689906e324686a")
-driver.find_element_by_xpath('//*[@id="list"]/tbody/tr[1]/td[2]/a').click()
-time.sleep(3)
-driver.find_element_by_id('returnButton').click()
-driver.find_element_by_xpath('//span[@class="rt right"]/child::a').click()
-addresses = driver.find_elements_by_xpath("//span[contains(@class,'text-icon')]")
-for i in addresses:
-    # if i.get_attribute('text') != '肖大兴':
-    i.click()
-    time.sleep(1)
-driver.find_element_by_xpath('//ul[@id="myTags"]//li//input').send_keys('wlxt@tsinghua.edu.cn')
-driver.implicitly_wait(3)
-driver.find_element_by_xpath('//ul[@id="myTags"]//li[2]/input').send_keys('xiesup@tsinghua.edu.cn')
-driver.implicitly_wait(3)
-driver.find_element_by_xpath('//input[@id="bt"]').send_keys('网络学堂学生端测试邮件！')
-driver.find_element_by_xpath('//input[@id="submitButton"]').click()
-time.sleep(3)
+# print('======测试课程邮件=====')
+# driver.find_element_by_link_text('课程邮件').click()
+# driver.find_element_by_xpath('//*[@id="list"]/tbody/tr[1]/td[2]/a').click()
+# time.sleep(3)
+# driver.find_element_by_id('returnButton').click()
+# driver.find_element_by_xpath('//span[@class="rt right"]/child::a').click()
+# addresses = driver.find_elements_by_xpath("//span[contains(@class,'text-icon')]")
+# for i in addresses:
+#     # if i.get_attribute('text') != '肖大兴':
+#     i.click()
+#     time.sleep(1)
+# driver.find_element_by_xpath('//ul[@id="myTags"]//li//input').send_keys('wlxt@tsinghua.edu.cn')
+# driver.find_element_by_xpath('//ul[@id="myTags"]//li//input').send_keys(Keys.ENTER)
+# driver.implicitly_wait(3)
+# # driver.find_element_by_xpath('//ul[@id="myTags"]//li[2]/input').send_keys('xiesup@tsinghua.edu.cn')
+# # driver.implicitly_wait(3)
+# driver.find_element_by_xpath('//input[@id="bt"]').send_keys('网络学堂学生端测试邮件！')
+# driver.find_element_by_xpath('//input[@id="submitButton"]').click()
+# time.sleep(3)
+
+print("===========测试课程文件============")
+driver.find_element_by_xpath('//a[@id="wlxt_kj_wlkc_kjxxb"]').click()
+driver.implicitly_wait(5)
 driver.quit()
 
 # print(len(addresses))
 
 
-# for add in addresses:
-#     add.click()
-# driver.find_element_by_name('i_user').send_keys('')
-# time.sleep(20)
 
 
 # driver.get(r'E:\163study\WebDriver--Python\upload.html')  # 文件的地址
