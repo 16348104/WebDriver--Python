a3e3d048cce3c515e34d1098fb1a857cd0db8d3a
16348104 <16348104@qq.com>
2019-04-29 15:39:58 +0800

    update 课程文件

--- wlxt/Student.py
+++ wlxt/Student.py
@@ -82,26 +82,41 @@ driver.switch_to.frame('playFrame')
 print('开始预览课件')
 try:
     Unable_preview = driver.find_element_by_xpath("//a[@class='downLoadFile']")
-    Video = driver.find_element_by_xpath("//button[@class='vjs-big-play-button']")
-    Audio = driver.find_element_by_css_selector("#mp3")
-    Word = driver.find_element_by_xpath("//body/a")
-    if Word.is_displayed():  # 文本文件
-        Word.click()
-        print('预览文本文件')
-    elif Audio:  # 音频文件
-        js = " var audio = document.getElementById('mp3');  audio.play();"
-        driver.execute_script(js).click()
-        print("预览音频文件")
-    elif Video:  # 视频文件
-        Video.click()
-        print("预览视频文件")
-    elif Unable_preview:  # 视频文件
-        Unable_preview.click()
-        print("预览视频文件")
 except NoSuchElementException as msg:
     print('此文件暂时无法预览', msg)
-
-
+else:
+    Unable_preview.click()
+    print('下载无法预览的文件')
+    time.sleep(3)
+try:
+    Video = driver.find_element_by_xpath("//button[@class='vjs-big-play-button']")
+except NoSuchElementException as msg:
+    print('暂无视频文件', msg)
+else:
+    Video.click()
+    print('预览视频文件')
+    time.sleep(3)
+try:
+    Audio = driver.find_element_by_css_selector("#mp3")
+except NoSuchElementException as msg:
+    print('暂无音频文件', msg)
+else:
+    js_audio = "var audio = document.getElementById('mp3');audio.play();"
+    driver.execute_script(js_audio)
+    print('预览音频文件')
+    time.sleep(3)
+try:
+    Word = driver.find_element_by_xpath("//body/a")
+except NoSuchElementException as msg:
+    print('暂无文本文件', msg)
+else:
+    Word.click()
+    time.sleep(1)
+    js_word = "alert('111');"
+    driver.execute_script(js_word)
+    print('预览文本文件')
+    # js = "document.getElementsByClassName('textLayer').scrollTop=700;console.log(document.getElementsByClassName('textLayer').scrollTop);"
+    time.sleep(3)
 driver.switch_to.parent_frame()
 time.sleep(5)
 print('=====课件测试完毕=====')
@@ -149,6 +164,9 @@ print('=====我的分组测试完毕=====')
 # driver.find_element_by_xpath('//ul[@id="myTags"]//li[3]/input').send_keys('dj1005@tsinghua.edu.cn')
 # driver.find_element_by_xpath('//ul[@id="myTags"]//li[3]//input').send_keys(Keys.ENTER)
 # time.sleep(1)
+# driver.find_element_by_xpath('//ul[@id="myTags"]//li[4]/input').send_keys('chc@tsinghua.edu.cn')
+# driver.find_element_by_xpath('//ul[@id="myTags"]//li[4]//input').send_keys(Keys.ENTER)
+# time.sleep(1)
 # js = "document.getElementById('bt').value = new Date().toLocaleString();"
 # val = driver.execute_script(js)
 # driver.find_element_by_xpath('//input[@id="bt"]').send_keys('网络学堂自动化测试结果—学生端功能正常')
@@ -161,13 +179,13 @@ print('=====我的分组测试完毕=====')
 # print('=====邮件测试完毕=====')
 
 ##################################################退出网络学堂############################################################
-driver.find_element_by_xpath("//a[contains(text(),'退出登录')]").click()
-time.sleep(2)
-driver.find_element_by_xpath("//div[contains(@class,'zeromodal-footer')]//button[contains(text(),'确定')]").send_keys(
-    Keys.ENTER)
-# js = "alert('12345')"
-# driver.execute_script(js)
-# driver.switch_to_alert().accept()
-# driver.switch_to.alert.accept()
-print('退出网络学堂')
+# driver.find_element_by_xpath("//a[contains(text(),'退出登录')]").click()
+# time.sleep(2)
+# driver.find_element_by_xpath("//div[contains(@class,'zeromodal-footer')]//button[contains(text(),'确定')]").send_keys(
+#     Keys.ENTER)
+# # js = "alert('12345')"
+# # driver.execute_script(js)
+# # driver.switch_to_alert().accept()
+# # driver.switch_to.alert.accept()
+# print('退出网络学堂')
 # driver.quit()
