6461b41992c96b5f8b1f2c5f609a9728d5e78f3d
xdx <16348104@qq.com>
2019-06-30 18:34:57 +0800

    update  Dmz社区

--- Dmzshequ/dmzshequ.py
+++ Dmzshequ/dmzshequ.py
@@ -15,11 +15,11 @@ browser = webdriver.Safari()
 browser.maximize_window()
 browser.get('http://www.dmzshequ.com')
 # browser.get('http://www.dmzshequ.com/plugin.php?id=dsu_paulsign:sign')#直接登录
-browser.implicitly_wait(2)
+browser.implicitly_wait(3)
 print('登录Dmz社区!')
 print(browser.title)
 print('浏览器:' + browser.name)
-browser.implicitly_wait(5)
+time.sleep(2)
 browser.find_element_by_xpath("//a[@class='deandengluanniu']").click()
 
 
@@ -29,19 +29,20 @@ def login(user, password):
 
 
 ## 登录
-login('zijing228', 'yu123456')
-# login('milometer', 'ustb55')
+# login('zijing228', 'yu123456')
+login('milometer', 'ustb55')
+# browser.implicitly_wait(2)
 browser.find_element_by_xpath('//button[@name="loginsubmit"]').click()
-browser.implicitly_wait(5)
+time.sleep(3)
 print('登录成功!')
 browser.refresh()  # 刷新
-browser.implicitly_wait(1)
 ##### 签到
+time.sleep(3)
 print("Dmz社区>", browser.find_element_by_xpath("//*[@id='pt']//a[2]").text)
 try:
     browser.find_element_by_xpath("//ul[@class='qdsmile']//following-sibling::li")
     # except NoSuchElementException as msg:
-except BaseException :
+except BaseException:
     print('今天已签到!')
     print(BaseException)
 else:
@@ -56,24 +57,31 @@ else:
     browser.find_element_by_xpath("//*[@id='qiandao']/table[1]/tbody/tr/td/div/a").click()
 
 ##### 摇一摇
+time.sleep(5)
 try:
     browser.find_element_by_xpath("//*[@id='zzza_tixing']/div[1]/div[1]/a")
 except BaseException:
     print('今天摇过了!')
     print(BaseException)
 else:
+    print(browser.find_element_by_xpath("//*[@id='zzza_tixing']/div[1]/div[1]/a").text)
     browser.find_element_by_xpath("//*[@id='zzza_tixing']/div[1]/div[1]/a").click()
     # browser.switch_to_alert()
-    browser.implicitly_wait(5)
+    time.sleep(5)
     try:
-        browser.find_element_by_xpath("//*[@id='zzza_go']").click()
+        print(browser.find_element_by_xpath("//*[@id='zzza_go']").text)
+        browser.find_element_by_xpath("//*[@id='zzza_go']").click()  # 摇金币
+        time.sleep(1)
     # except UnexpectedAlertPresentException as msg_alert:
     except BaseException:
         print(BaseException)
-        print(browser.switch_to.alert.text)
+        print(browser.switch_to.alert.text)  # 如果签到不成功，接受摇金币时弹出alter
         browser.switch_to.alert.accept()
-    browser.implicitly_wait(3)
-    browser.find_element_by_xpath('//*[@id="yyl-random-box"]/div[1]').click()
+    else:
+        pass
+        # time.sleep(5)
+        # browser.find_element_by_xpath('//*[@id="yyl-random-box"]/div[1]').click()
+time.sleep(5)
 print('今天任务已完成!')
 current_time = time.strftime("%y-%m-%d %H:%M:%S", time.localtime(time.time()))
 print('在', current_time, '退出Dmz社区')
