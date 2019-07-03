424ca4eb40c9585cc3f47a4a73f067fe0a7fed32
IDEA <16348104@QQ.com>
2019-07-03 23:28:26 +0800

    update   chromedriver

--- Dmzshequ/connect.py
+++ Dmzshequ/connect.py
@@ -7,9 +7,9 @@ browser = webdriver.Chrome()
 browser.get('https://net.tsinghua.edu.cn')
 browser.maximize_window()
 time.sleep(2)
-print("联接校园网！")
 
 
+# 连接校园网
 def login(user, password):
     browser.find_element_by_id('uname').send_keys(user)
     browser.find_element_by_id('pass').send_keys(password)
@@ -17,14 +17,15 @@ def login(user, password):
 
 
 login('', "")
-try:
-    print(browser.switch_to.alert.text)
-    browser.switch_to.alert.accept()
-except BaseException:
-    pass
-# browser.get('http://net.tsinghua.edu.cn')
-time.sleep(10)
+time.sleep(1)
+print(browser.switch_to.alert.text)
+browser.switch_to.alert.accept()
+time.sleep(100)
+# 断网
+browser.refresh()
+time.sleep(1)
 js = "window.do_logout();"
 browser.execute_script(js)
+time.sleep(1)
 print(browser.switch_to.alert.text)
 browser.switch_to.alert.accept()
