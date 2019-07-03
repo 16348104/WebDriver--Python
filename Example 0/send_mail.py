df6f66f5553f52226126093b7649794ca268b880
16348104 <16348104@qq.com>
2019-07-03 15:06:20 +0800

    update  send mail

--- Example 0/send_mail.py
+++ Example 0/send_mail.py
@@ -3,22 +3,24 @@ from email.header import Header
 from email.mime.text import MIMEText
 
 smtpsever = 'smtp.126.com'
-password = 'xdx2019'
+password = input("input:")
+# password = 'xdx2019'
 user = 'xiaodaxing@126.com'
 sender = 'xiaodaxing@126.com'
-receiver1 = 'yumj@tsinghua.edu.cn'
-receiver2 = 'pkucrjy2013@163.com'
+# receiver = ['yumj@tsinghua.edu.cn'], ['xdx2016@tsinghua.edu.cn']
+# receiver = '16348104@qq.com'
+receiver = 'pkucrjy2013@163.com'
 subject = 'Dmz'
-msg = MIMEText('<html><h3>Hello,<br>Our task is done.</h3></html>', 'html', 'utf-8')
+mail_msg = '<html><h3>Hello,<br>Our task is done.</h3></html>'
+msg = MIMEText(mail_msg, 'html', 'utf-8')
 msg['Subject'] = Header(subject, 'utf-8')
 msg['From'] = user
-msg['To'] = receiver1
-msg['To'] = receiver2
+msg['To'] = receiver
+# msg['To'] = receiver2
 smtp = smtplib.SMTP()
 smtp.set_debuglevel(1)
 smtp.connect(smtpsever, 25)
 smtp.login(user, password)
-smtp.sendmail(sender, receiver1, msg.as_string())
-smtp.sendmail(sender, receiver2, msg.as_string())
+smtp.sendmail(sender, receiver, msg.as_string())
 smtp.quit()
 print('email has send out!')
