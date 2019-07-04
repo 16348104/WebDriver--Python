8d8e9b8816b41ed638d3ef2ee884397ccfdda6d9
16348104 <16348104@qq.com>
2019-07-04 11:31:10 +0800

    update  send mail

--- Dmzshequ/connect.py
+++ Dmzshequ/connect.py
@@ -1,9 +1,10 @@
 import time
+import os
 from selenium import webdriver
 
-# browser = webdriver.Firefox()
+browser = webdriver.Firefox()
 # # browser = webdriver.Ie()
-browser = webdriver.Chrome()
+# browser = webdriver.Chrome()
 browser.get('https://net.tsinghua.edu.cn')
 browser.maximize_window()
 time.sleep(2)
@@ -13,19 +14,30 @@ time.sleep(2)
 def login(user, password):
     browser.find_element_by_id('uname').send_keys(user)
     browser.find_element_by_id('pass').send_keys(password)
-    browser.find_element_by_id('connect').click()
+    # browser.find_element_by_id('connect').click()
+    time.sleep(1)
+    js_connect = 'do_login();'
+    browser.execute_script(js_connect)
+    time.sleep(1)
+    print(browser.switch_to.alert.text)
+    browser.switch_to.alert.accept()
 
 
-login('', "")
-time.sleep(1)
-print(browser.switch_to.alert.text)
-browser.switch_to.alert.accept()
-time.sleep(100)
+try:
+    login('', "")
+except BaseException:
+    print('已连网')
+time.sleep(2)
+date = browser.find_element_by_xpath('//*[@id="usage_flux"]').text
+print(date)
+os.system("python E:/163study/WebDriver--Python/Example/send_mail.py")
+time.sleep(60)
 # 断网
-browser.refresh()
 time.sleep(1)
-js = "window.do_logout();"
-browser.execute_script(js)
+js_disconnect = "do_logout();"
+browser.execute_script(js_disconnect)
 time.sleep(1)
 print(browser.switch_to.alert.text)
 browser.switch_to.alert.accept()
+time.sleep(2)
+browser.quit()
--- Example/send_mail.py
+++ Example/send_mail.py
@@ -3,15 +3,17 @@ from email.header import Header
 from email.mime.text import MIMEText
 
 smtpsever = 'smtp.126.com'
-password = input("input:")
-# password = 'xdx2019'
+# password = input("input:")
+password = 'xdx2019'
 user = 'xiaodaxing@126.com'
 sender = 'xiaodaxing@126.com'
 # receiver = ['yumj@tsinghua.edu.cn'], ['xdx2016@tsinghua.edu.cn']
 # receiver = '16348104@qq.com'
 receiver = 'pkucrjy2013@163.com'
-subject = 'Dmz'
-mail_msg = '<html><h3>Hello,<br>Our task is done.</h3></html>'
+# subject = 'Dmz'
+subject = 'Network Connect'
+# mail_msg = '<html><h3>Hello,<br>The network is connected.</h3></html>'
+mail_msg = '<html><h3>Hello,<br>connected.</h3></html>'
 msg = MIMEText(mail_msg, 'html', 'utf-8')
 msg['Subject'] = Header(subject, 'utf-8')
 msg['From'] = user
@@ -23,4 +25,4 @@ smtp.connect(smtpsever, 25)
 smtp.login(user, password)
 smtp.sendmail(sender, receiver, msg.as_string())
 smtp.quit()
-print('email has send out!')
+print('Email has send out!')
