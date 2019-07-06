6542e94da468138e51c81dc965be1e302d3b6349
IDEA <16348104@QQ.com>
2019-07-06 19:21:25 +0800

    update   chromedriver

--- wlxt/Teacher.py
+++ wlxt/Teacher.py
@@ -447,23 +447,26 @@ time.sleep(2)
 driver.execute_script("document.documentElement.scrollTop = 10000;")  # 滚动条
 # CKeditor音频文件
 driver.find_element_by_xpath("//a[@id='cke_41']").click()
-time.sleep(3)
+time.sleep(1)
 # # win32gui
-# dialog = win32gui.FindWindow('#32770', '打开')  # 对话框
-# ComboBoxEx32 = win32gui.FindWindowEx(dialog, 0, 'ComboBoxEx32', None)
-# ComboBox = win32gui.FindWindowEx(ComboBoxEx32, 0, 'ComboBox', None)
-# Edit = win32gui.FindWindowEx(ComboBox, 0, 'Edit', None)  # 上面三句依次寻找对象，直到找到输入框Edit对象的句柄
-# button = win32gui.FindWindowEx(dialog, 0, 'Button', None)  # 确定按钮Button
-# time.sleep(2)
-# win32gui.SendMessage(Edit, win32con.WM_SETTEXT, 0, "D:\Artists.mp3")  # 往输入框输入绝对地址D:\
-# time.sleep(2)
+dialog = win32gui.FindWindow('#32770', '打开')  # 对话框
+ComboBoxEx32 = win32gui.FindWindowEx(dialog, 0, 'ComboBoxEx32', None)
+ComboBox = win32gui.FindWindowEx(ComboBoxEx32, 0, 'ComboBox', None)
+Edit = win32gui.FindWindowEx(ComboBox, 0, 'Edit', None)  # 上面三句依次寻找对象，直到找到输入框Edit对象的句柄
+button = win32gui.FindWindowEx(dialog, 0, 'Button', None)  # 确定按钮Button
+win32gui.SendMessage(Edit, win32con.WM_SETTEXT, None, "mp4")  # 往输入框输入绝对地址D:\
 # win32gui.SendMessage(dialog, win32con.WM_COMMAND, 1, button)  # 按button
-if __name__ == "__main__":
-    WinUpLoadFile().winUpLoadFile("D:\mov.mp4", "打开")
+win32gui.PostMessage(dialog, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
+win32gui.PostMessage(dialog, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
+
+
+# if __name__ == "__main__":
+#     WinUpLoadFile().winUpLoadFile("D:\mov.mp4", "打开")
 time.sleep(5)
 
 # 上传附件
 driver.find_element_by_xpath('//*[@id="fileupload"]').send_keys(r'D:/listening.pdf')  # modify
+# driver.find_element_by_xpath('//*[@id="editFormId"]/div[2]/div/div[1]/label/div/span').click()
 time.sleep(1)
 # driver.find_element_by_xpath('//*[@id="saveBtn"]').click()
 time.sleep(2)
