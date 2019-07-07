eb776819f5c3a2d6c51d8d1e64563dc8ab9aea62
IDEA <16348104@QQ.com>
2019-07-07 22:02:31 +0800

    update   chromedriver

--- wlxt/Teacher.py
+++ wlxt/Teacher.py
@@ -113,31 +113,50 @@ time.sleep(2)
 # print('=====公告测试完毕=====')
 # time.sleep(4)
 ######################################################课程文件##########################################################
-# print('=====测试课程文件=====')
-# driver.find_element_by_xpath("//a[@id='wlxt_kj_wlkc_kjxxb']").click()
-# time.sleep(2)
-# print('=====发课件=====')
-# driver.find_element_by_xpath('//span[@class="rt right"]/child::a').click()  # 上课件
-# time.sleep(1)
+print('=====测试课程文件=====')
+driver.find_element_by_xpath("//a[@id='wlxt_kj_wlkc_kjxxb']").click()
+time.sleep(2)
+print('=====发课件=====')
+driver.find_element_by_xpath('//span[@class="rt right"]/child::a').click()  # 上课件
+time.sleep(1)
+
+driver.find_element_by_xpath('//span[contains(text(),"添加附件（最大1G）")]').click()
+time.sleep(1)
+dialog = win32gui.FindWindow('#32770', '打开')
+ComboBoxEx32 = win32gui.FindWindowEx(dialog, 0, 'ComboBoxEx32', None)
+ComboBox = win32gui.FindWindowEx(ComboBoxEx32, 0, 'ComboBox', None)
+Edit = win32gui.FindWindowEx(ComboBox, 0, 'Edit', None)  # 上面三句依次寻找对象，直到找到输入框Edit对象的句柄
+button = win32gui.FindWindowEx(dialog, 0, 'Button', None)  # 确定按钮Button
+time.sleep(1)
+win32gui.SendMessage(Edit, win32con.WM_SETTEXT, None, 'D:\mov.mp4')  # 往输入框输入绝对地址D:\
+win32gui.SendMessage(dialog, win32con.WM_COMMAND, 1, button)  # 按button
+# # win32gui.PostMessage(dialog, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
+# # win32gui.PostMessage(dialog, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
+print(dialog)
+# print("%x" % ComboBox)
+print(Edit)
+print(button)
+time.sleep(5)
+
 # js = "document.getElementById('fileupload').style.display=\'block\'"
 # driver.execute_script(js)
 # driver.find_element_by_name("bt").send_keys("测试课件" + ticks)
 # driver.find_element_by_xpath("//div[@class='list']//label[1]").click()  # 重要标记
 # driver.find_element_by_name("fileupload").send_keys("D:/Artists.mp3")  # modify
-# # driver.find_element_by_id('fileupload').send_keys(
-# #     r'/Users/xdx/PycharmProjects/WebDriver--Python/wlxt/readme.txt')  # mac上传文件
-# time.sleep(5)
-# driver.find_element_by_id("sub").click()
-# time.sleep(1)
-# try:
-#     driver.find_element_by_css_selector(
-#         "body > div.zeromodal-container.alert > div.zeromodal-body > div.zeromodal-title1")
-# except NoSuchElementException as msg:
-#     print('截图', msg)
-#     driver.get_screenshot_as_file("C:/Users/zb/Downloads/FireShot/" + time_format() + 'KJ' + ".png")  # 截图modify
-# else:
-#     print('弹框结果:' + driver.find_element_by_css_selector(
-#         "body > div.zeromodal-container.alert > div.zeromodal-body > div.zeromodal-title1").text)
+# driver.find_element_by_id('fileupload').send_keys(
+#     r'/Users/xdx/PycharmProjects/WebDriver--Python/wlxt/readme.txt')  # mac上传文件
+time.sleep(5)
+driver.find_element_by_id("sub").click()
+time.sleep(1)
+try:
+    driver.find_element_by_css_selector(
+        "body > div.zeromodal-container.alert > div.zeromodal-body > div.zeromodal-title1")
+except NoSuchElementException as msg:
+    print('截图', msg)
+    driver.get_screenshot_as_file("C:/Users/zb/Downloads/FireShot/" + time_format() + 'KJ' + ".png")  # 截图modify
+else:
+    print('弹框结果:' + driver.find_element_by_css_selector(
+        "body > div.zeromodal-container.alert > div.zeromodal-body > div.zeromodal-title1").text)
 # print('=====查看课件详情=====')
 # driver.find_element_by_xpath("//a[@id='wlxt_kj_wlkc_kjxxb']").click()
 # time.sleep(2)
