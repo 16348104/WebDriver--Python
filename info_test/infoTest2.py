25ae4e9f78ead407639ad88051f3f2a1c214ecd1
16348104 <16348104@qq.com>
2019-08-28 17:26:03 +0800

    update  infoen

--- info_test/infoTest2.py
+++ info_test/infoTest2.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from public2 import LoginInfo
 from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 
 class LoginTest():
@@ -14,22 +15,22 @@ class LoginTest():
         # driver.get('http://101.6.28.150:29009')
         # self.driver.get('http://eng.info.tisnghua.edu.cn')
         self.driver.maximize_window()
-        time.sleep(2)
+        time.sleep(3)
         # driver.set_window_size(1080*760)
         # self.driver.implicitly_wait(10)
 
     # 教师登录
     def test_admin_login(self):
-        username = '2011990118'
+        user = '2011990118'
         password = 'a123456'
-        LoginInfo().user_login(self.driver, username, password)
+        LoginInfo().user_login(self.driver, user, password)
         time.sleep(2)
 
     # 本科登录
     def test_bachelor_login(self):
-        username = '2017013478'
+        user = '2017013478'
         password = 'a123456'
-        LoginInfo().user_login(self.driver, username, password)
+        LoginInfo().user_login(self.driver, user, password)
 
     # 硕士登录
     def test_master_login(self):
@@ -39,18 +40,14 @@ class LoginTest():
 
     # 退出系统
     def test_logout(self):
-        username = '2017013478'
+        user = '2017013478'
         password = 'a123456'
-        LoginInfo().user_login(self.driver, username, password)
+        LoginInfo().user_login(self.driver, user, password)
         time.sleep(5)
         # LoginInfo().user_login(self.driver)
 
     # 测试enginfo
     def test_enginfo(self):
-        username = '2017013478'
-        password = 'a123456'
-        LoginInfo().user_login(self.driver, username, password)
-        time.sleep(3)
         print("======进入info=====")
         print('测试浏览器:' + self.driver.name)
         # driver.get('http://101.6.28.150:29009')
@@ -61,24 +58,19 @@ class LoginTest():
         # # 切换【第二个窗口】
         windows = self.driver.window_handles
         # # 切换到新窗口
-        # driver.switch_to.window(windows[1])
-        # window_2 = driver.current_window_handle
-        # print('所有句柄:', windows)
-        # print("当前窗口：", window_2)
+        self.driver.switch_to.window(windows[1])
+        window_2 = self.driver.current_window_handle
+        print('所有句柄:', windows)
+        print("当前窗口：", window_2)
         time.sleep(3)
+        self.driver.close()
         # 切换到第1个窗口
         self.driver.switch_to.window(windows[0])
         window_2 = self.driver.current_window_handle
         print("当前窗口：", window_2)
-
-        # def login(user, password):
-        #     driver.find_element_by_id('user').send_keys(user)
-        #     driver.find_element_by_id('pass').send_keys(password)
-        #     time.sleep(1)
-        #     driver.find_element_by_id('loginButtonId').send_keys(Keys.ENTER)
-        #
-        #
-        # login('2011990118', 'a123456')
+        username = '2017013478'
+        password = 'a123456'
+        LoginInfo().user_login(self.driver, username, password)
         time.sleep(2)
         print("======Edit Personal Details======")
         self.driver.find_element_by_xpath("//*[@class='btn']").click()
@@ -113,16 +105,11 @@ class LoginTest():
         ActionChains(self.driver).move_to_element(above).perform()
         above.click()
         time.sleep(3)
-        js_logout = "beforeLogout();"
-        self.driver.execute_script(js_logout)
-        # time.sleep(1)
-        self.driver.find_element_by_xpath("//*[@class='btn btn-sub']").send_keys(Keys.ENTER)
-        print('=====退出info=====')
-        self.driver.delete_all_cookies()
-        self.driver.quit()
+        LoginInfo.user_logout(self, self.driver)
 
 
 # 执行测试
 # LoginTest().test_admin_login()
 # LoginTest().test_bachelor_login()
-LoginTest().test_logout()
+# LoginTest().test_logout()
+LoginTest().test_enginfo()
--- info_test/public2.py
+++ info_test/public2.py
@@ -13,11 +13,11 @@ class LoginInfo():
         time.sleep(3)
 
     # 退出
-    def user_logout(self, driver, user, password):
-        driver.find_element_by_id('user').send_keys(user)
-        driver.find_element_by_id('pass').send_keys(password)
-        driver.find_element_by_id('loginButtonId').send_keys(Keys.ENTER)
-        time.sleep(5)
+    def user_logout(self, driver):
+        # driver.find_element_by_id('user').send_keys(user)
+        # driver.find_element_by_id('pass').send_keys(password)
+        # driver.find_element_by_id('loginButtonId').send_keys(Keys.ENTER)
+        # time.sleep(5)
         js_logout = "beforeLogout();"
         driver.execute_script(js_logout)
         # time.sleep(1)
