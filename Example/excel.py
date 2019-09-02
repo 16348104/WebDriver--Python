1bcd4edd7b3529fdeebea5ca2b140723158a5a3b
16348104 <16348104@qq.com>
2019-09-02 10:20:33 +0800

    update  pythonexcel

--- Example/excel.py
+++ Example/excel.py
@@ -33,19 +33,20 @@ def set_style(name, height, bold=False):
 # 写Excel
 def write_excel():
     f = xlwt.Workbook()
-    sheet1 = f.add_sheet('个院系链接', cell_overwrite_ok=True)
+    sheet1 = f.add_sheet('各院系链接', cell_overwrite_ok=True)
     row0 = ["模块", "链接"]
     colum0 = ["张三", "李四"]
     colum1 = ["Python", "java", "C#", "VB"]
 
     # 写第一行
     for i in range(0, len(row0)):
-        sheet1.write(0, i, row0[i], set_style('TimesNewRoman', 220))
+        sheet1.write(0, i, row0[i])
     # 写第一列
     for i in range(0, len(colum0)):
-        sheet1.write(i + 1, 0, colum0[i], set_style('TimesNewRoman', 220))
+        sheet1.write(i + 1, 0, colum0[i])
+    # 写第二列
     for i in range(0, len(colum1)):
-        sheet1.write(i + 1, 1, colum1[i], set_style('TimesNewRoman', 220))
+        sheet1.write(i + 1, 1, colum1[i])
         # sheet1.write(1, 3, '2006/12/12')
         # sheet1.write_merge(6, 6, 1, 3, '未知')  # 合并行单元格
         # sheet1.write_merge(1, 2, 3, 3, '打游戏')  # 合并列单元格
