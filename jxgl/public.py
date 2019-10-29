6f25125eb7d04f8af8ebee6603135b2eb9ba35ad
16348104 <16348104@qq.com>
2019-10-29 17:18:26 +0800

    update  jxgl

--- jxgl/public.py
+++ jxgl/public.py
@@ -1,3 +1,8 @@
+import time
+from selenium import webdriver
 class LoginJXGL():
-# 登录
-def userlogin():
+    # 登录
+    def userlogin(self, driver, user, password):
+        driver.find_element_by_xpath('//*[@id="userName').send_keys('user')
+        driver.find_element_by_id('pass').send_keys('password')
+
