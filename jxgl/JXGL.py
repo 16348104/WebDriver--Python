52211e0cccb8b17756ff3e3d3d6bdc21a2e6ac35
16348104 <16348104@qq.com>
2019-10-31 18:35:54 +0800

    update  jxgl

--- jxgl/JXGL.py
+++ jxgl/JXGL.py
@@ -1,9 +1,9 @@
 import time
 
-from openpyxl import Workbook
+from selenium import webdriver
+from openpyxl import *
 import xlrd
 # //table[@class='table table-bordered']//tbody//tr[1]//td[3]//ul[1]//li
-from selenium import webdriver
 from public import LoginJXGL
 
 
@@ -16,7 +16,27 @@ class Test_JXGL():
         # self.driver = webdriver.Chrome(executable_path='/Users/xdx/PycharmProjects/WebDriver--Python/chromedriver')  # MacOS
 
         # Eng_info模拟环境
-        self.driver.get('http://infoen.syx.thcic.cn')
+        self.driver.get('http://info.syx.thcic.cn')
         self.driver.maximize_window()
-        print("======进入info学生端=====")
-        time.sleep(4)
+        time.sleep(2)
+
+    # 登录
+    def login(self):
+        wb = load_workbook(r'E:\sample.xlsx', read_only=True)
+        my_sheet = wb.worksheets[0]
+        A2 = my_sheet["A2"]
+        B2 = my_sheet["B2"]
+        # 用cell 函数
+        # user = my_sheet.cell(row=2, column=1)
+        # password = my_sheet.cell(row=2, column=2)
+        # print(A2.value, B2.value)
+        user = A2.value
+        password = B2.value
+        # print(user, password)
+        LoginJXGL.userlogin(self.driver, user, password)
+        time.sleep(2)
+        print("登录info")
+
+
+# 执行测试
+Test_JXGL().login()
--- jxgl/public.py
+++ jxgl/public.py
@@ -1,8 +1,12 @@
 import time
 from selenium import webdriver
+
+
 class LoginJXGL():
     # 登录
     def userlogin(self, driver, user, password):
-        driver.find_element_by_xpath('//*[@id="userName').send_keys('user')
-        driver.find_element_by_id('pass').send_keys('password')
-
+        print(user, password)
+        driver.find_element_by_xpath('//*[@id="userName').send_keys(user)
+        driver.find_element_by_xpath('//input[@name="password"]').send_keys(password)
+        time.sleep(2)
+        driver.find_element_by_xpath('//td[@class="but"]/input').click()
