0a1178c9617b35efe60599207290a89a2c7ba2da
16348104 <16348104@qq.com>
2019-11-21 14:36:55 +0800

    connect  IEdriver32

--- Dmzshequ/connect.py
+++ Dmzshequ/connect.py
@@ -3,7 +3,7 @@ import os
 from selenium import webdriver
 
 # browser = webdriver.Firefox()
-browser = webdriver.Ie(executable_path='E:\Program Files\Python\Python37\IEDriverServer.exe')
+browser = webdriver.Ie()
 # browser = webdriver.Chrome()
 browser.get('https://net.tsinghua.edu.cn')
 browser.maximize_window()
@@ -12,8 +12,8 @@ time.sleep(2)
 
 # 连接校园网
 def login(user, password):
-    browser.find_element_by_id('uname').send_keys(user)
-    browser.find_element_by_id('pass').send_keys(password)
+    browser.find_element_by_id('uname').send_keys('xdx2016')
+    browser.find_element_by_id('pass').send_keys('')
     # browser.find_element_by_id('connect').click()
     time.sleep(1)
     js_connect = 'do_login();'
