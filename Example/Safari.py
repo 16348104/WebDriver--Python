f6bfbd1837264d79c7dabcdba5f8bddb4e46fee7
IDEA <16348104@QQ.com>
2019-11-23 16:25:44 +0800

    update weibo

--- Example/Safari.py
+++ Example/Safari.py
@@ -5,7 +5,8 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Safari()
+browser = webdriver.Chrome()
+# browser = webdriver.Safari()
 # browser = webdriver.Firefox(executable_path='/Users/xdx/PycharmProjects/WebDriver--Python/geckodriver')
 browser.get('http://weibo.com')
 browser.maximize_window()
