b15f64a3cf6a17d14833ee0f5bf998109c42514c
16348104 <16348104@qq.com>
2020-05-28 16:06:49 +0800

    dmz3

--- Dmzshequ/dmz.py
+++ Dmzshequ/dmz.py
@@ -3,6 +3,8 @@ import smtplib
 from selenium import webdriver
 from email.header import Header
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 from selenium.common.exceptions import NoSuchElementException, UnexpectedAlertPresentException, TimeoutException, \
     ElementNotInteractableException
 browser = webdriver.Firefox()
@@ -13,15 +15,16 @@ time.sleep(5)
 ##发邮件
 print('去发邮件!')
 smtpsever = 'mail.tsinghua.edu.cn'
+user = 'yumj@tsinghua.edu.cn'
 password = ''
-user = 'xdx2016@tsinghua.edu.cn'
-sender = 'XiaoDaXing2019'
-receiver =[ 'yumj@tsinghua.edu.cn']
+sender = 'yumj@tsinghua.edu.cn'
+receiver = ['yumj@tsinghua.edu.cn']
+subject = 'DMZ摇一摇'
 mail_msg = '<html><h3>Hello,<br>Our task is done.</h3></html>'
 msg = MIMEText(mail_msg, 'html', 'utf-8')
 msg['Subject'] = Header(subject, 'utf-8')
 msg['From'] = user
-msg['To'] = receiver
+# msg['To'] = receiver
 smtp = smtplib.SMTP()
 smtp.set_debuglevel(1)
 smtp.connect(smtpsever, 25)
@@ -29,3 +32,4 @@ smtp.login(user, password)
 smtp.sendmail(sender, receiver, msg.as_string())
 smtp.quit()
 print('Email has send out!')
+# browser.quit()
